abce422b7696a0040271a4ae7b9b1f3b01ce5ede
DeenoDev <deyanoklaasen@outlook.com>
2022-11-13 22:37:47 +0200

    template.html file added.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import generateJoke from "./generateJoke";
+// DEMO JOKE APP
+import generateJoke from './generateJoke'
+import './styles/main.scss'
+import laughing from './assets/laughing.svg'
 
-console.log(generateJoke());
+const laughImg = document.getElementById('laughImg')
+laughImg.src = laughing
+
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
